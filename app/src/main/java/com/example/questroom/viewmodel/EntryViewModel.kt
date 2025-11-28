48647da45a4d46c69112d39a5b6ad6c443ae627a
HafizZada16 <hafizzada16@gmail.com>
2025-11-28 18:31:22 +0700

    Add DetailSiswa.toSiswa

--- app/src/main/java/com/example/questroom/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/questroom/viewmodel/EntryViewModel.kt
@@ -44,3 +44,14 @@ data class DetailSiswa(
     val alamat: String = "",
     val telpon: String = "",
 )
+
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+
+
+
