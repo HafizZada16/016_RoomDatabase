9586cb7db338394dd0c17a9872a6cfaadc2fa1cb
HafizZada16 <hafizzada16@gmail.com>
2025-11-28 19:09:23 +0700

    Fix ViewModelProvider PenyediaViewModel.kt

--- app/src/main/java/com/example/questroom/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questroom/viewmodel/provider/PenyediaViewModel.kt
@@ -1,12 +1,13 @@
 package com.example.questroom.viewmodel.provider
 
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import androidx.lifecycle.viewmodel.ViewModelProvider
 import com.example.questroom.repositori.AplikasiSiswa
 import com.example.questroom.viewmodel.EntryViewModel
 import com.example.questroom.viewmodel.HomeViewModel
+
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
