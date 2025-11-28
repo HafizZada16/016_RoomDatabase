77616da648d7598d3ec87dec45995395a17dd301
HafizZada16 <hafizzada16@gmail.com>
2025-11-28 18:59:34 +0700

    Add SiswaApp MainActivity.kt

--- app/src/main/java/com/example/questroom/MainActivity.kt
+++ app/src/main/java/com/example/questroom/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questroom.ui.theme.QuestRoomTheme
+import com.example.questroom.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestRoomTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    SiswaApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
