77c86fc4d2abaedf28798ee8cb1d30c8569eba34
HafizZada16 <hafizzada16@gmail.com>
2025-11-28 18:23:51 +0700

    Add validasiInput EntryViewModel.kt

--- app/src/main/java/com/example/questroom/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/questroom/viewmodel/EntryViewModel.kt
@@ -3,12 +3,22 @@ package com.example.questroom.viewmodel
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.questroom.repositori.RepositoriSiswa
+import com.example.questroom.room.Siswa
 
-class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): viewmodel()
-    {
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): viewmodel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
-            private set
+        private set
 
-            private fun
+    private fun validasiInput(
+        uiState: DetailSiswa = uiStateSiswa.detailSiswa
+    ): Boolean {
+
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
     }
+
+
+}
