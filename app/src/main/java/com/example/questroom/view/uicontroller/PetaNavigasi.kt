eab390b116228b6b0ae1ed8b1b84b648b5b58cc1
HafizZada16 <hafizzada16@gmail.com>
2025-11-28 18:51:49 +0700

    Add PetaNavigasi.kt

---
+++ app/src/main/java/com/example/questroom/view/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,22 @@
+package com.example.questroom.view.uicontroller
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController(),
+             modifier: Modifier) {
+    HostNavigasi(navController = navController)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost
+)
