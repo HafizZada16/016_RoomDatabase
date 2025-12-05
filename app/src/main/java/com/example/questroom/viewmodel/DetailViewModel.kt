d11f3166d2a304d3210f8f9865fa8c58ed5b76fc
HafizZada16 <hafizzada16@gmail.com>
2025-12-05 15:09:20 +0700

    Menambah UI state for ItemDetailsScreen

--- app/src/main/java/com/example/questroom/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questroom/viewmodel/DetailViewModel.kt
@@ -11,8 +11,6 @@ import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 
-private val DetailSiswaUiState.detailSiswa: Any
-
 class DetailViewModel (
     savedStateHandle: SavedStateHandle,
     private val repositoriSiswa: RepositoriSiswa) : ViewModel(){
@@ -38,3 +36,10 @@ class DetailViewModel (
         private const val TIMEOUT_MILLIS = 5_000L
     }
 }
+
+/**
+ * UI state for ItemDetailsScreen
+ */
+data class DetailSiswaUiState(
+    val detailSiswa: DetailSiswa = DetailSiswa()
+)
