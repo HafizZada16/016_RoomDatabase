53646a8f798cb206899153cb9b29ffd1caf35696
HafizZada16 <hafizzada16@gmail.com>
2025-12-05 15:18:39 +0700

    Menambah BodyDetailDataSiswa DetailSiswaScreen

--- app/src/main/java/com/example/questroom/view/HalamanDetailSiswa.kt
+++ app/src/main/java/com/example/questroom/view/HalamanDetailSiswa.kt
@@ -1,7 +1,10 @@
 package com.example.questroom.view
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -9,6 +12,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -17,6 +21,7 @@ import com.example.questroom.R
 import com.example.questroom.view.route.DestinasiDetailSiswa
 import com.example.questroom.viewmodel.DetailViewModel
 import com.example.questroom.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -49,5 +54,28 @@ fun DetailSiswaScreen(
                     contentDescription = stringResource(R.string.update),
                 )
             }
-        },
+        }, modifier = modifier
+    ) { innerPadding ->
+        val uiState = viewModel.uiDetailState.collectAsState()
+        val coroutineScope = rememberCoroutineScope()
+        BodyDetailDataSiswa(
+            detailSiswaUiState = uiState.value,
+            onDelete = { coroutineScope.launch {
+                viewModel.deleteSiswa()
+                navigateBack()
+            }},
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+        )
+    }
+}
+
+@Composable
+fun BodyDetailDataSiswa(
+    detailSiswaUiState: DetailSiswaUiState,
+    onDelete: () -> launch,
+    modifier: verticalScroll
+) {
+    TODO("Not yet implemented")
 }
