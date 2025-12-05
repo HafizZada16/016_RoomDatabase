5ff4e4d33890eb09288c29e147f3c596e6d91fc9
HafizZada16 <hafizzada16@gmail.com>
2025-12-05 15:16:51 +0700

    Menambah Scaffold DetailSiswaScreen

--- app/src/main/java/com/example/questroom/view/HalamanDetailSiswa.kt
+++ app/src/main/java/com/example/questroom/view/HalamanDetailSiswa.kt
@@ -1,9 +1,20 @@
 package com.example.questroom.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questroom.R
+import com.example.questroom.view.route.DestinasiDetailSiswa
 import com.example.questroom.viewmodel.DetailViewModel
 import com.example.questroom.viewmodel.provider.PenyediaViewModel
 
@@ -14,4 +25,29 @@ fun DetailSiswaScreen(
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){}
+){
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiDetailSiswa.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        },
+        floatingActionButton = {
+            val uiState = viewModel.uiDetailState.collectAsState()
+            FloatingActionButton(
+                onClick = {
+                    //navigateToEditItem(uiState.value.detailSiswa.id)
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.update),
+                )
+            }
+        },
+}
