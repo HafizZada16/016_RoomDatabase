74263069de3a2371b8c28ddf05c14cd304024319
HafizZada16 <hafizzada16@gmail.com>
2025-12-05 15:04:36 +0700

    Menambah object DestinasiDetailSiswa

--- app/src/main/java/com/example/questroom/view/route/DestinasiDetailSiswa.kt
+++ app/src/main/java/com/example/questroom/view/route/DestinasiDetailSiswa.kt
@@ -1,2 +1,10 @@
 package com.example.questroom.view.route
 
+import com.example.questroom.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
